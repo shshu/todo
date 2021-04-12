166daa0f20d936fb8c358accd56e51ce14aa100f
sahar.shukrun <sahar.shukrun@sisense.com>
2021-04-12 21:19:06 +0300

    Add todo DB load and /api/test endpoint

--- backend/app.py
+++ backend/app.py
@@ -8,14 +8,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
-app.config.from_object('config.DevConfig')
+app.config.from_object('config.Config')
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-from api import login
+from api import login, todo
 # from models.task import Task
-from models import user
+from models import task, user
 
 
 # get_jwt_identity()
@@ -25,7 +25,12 @@ def home():
     username = get_jwt_identity()
     return f"Hello World! {username}"
 
+@app.route("/api/test")
+def test():
+    return f"alive!"
+
 # export FLASK_APP=backend/app.py
+# export FLASK_ENV=development
 # flask run
 # curl -X GET localhost:5000/ -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYxNzczOTI1OSwianRpIjoiYWIxY2QzNWEtNTk5MS00MjdiLThkYTItOTAwZGE1NGNkMjkwIiwibmJmIjoxNjE3NzM5MjU5LCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoiYWRtaW4iLCJleHAiOjE2MTc3NDAxNTl9.KEPXFvgGVX_ouZYYRK2G8KOmHyFlGPedh9TD-0uk35Y"
 
